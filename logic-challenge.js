1da33700faa28656612691946621022abaec7f01
Shahyan Sajid <shahyansajid@users.noreply.github.com>
2024-07-04 04:42:16 -0500

    Fix logic challenge formatting

--- logic-challenge.js
+++ logic-challenge.js
@@ -1,28 +1,32 @@
-if (clientData.employment_status === ENUMS.EMPLOYMENT_STATUSES.UNEMPLOYED) {
-		if (clientData.has_unemployment_benefits === null || clientData.has_unemployment_benefits === ''
-			|| clientData.has_unemployment_benefits === undefined) {
-			return badRequestHandler(res, 'Error: Unemployment benefits status is not specified');
-		} else if (typeof clientData.has_unemployment_benefits !== 'boolean') {
-			return badRequestHandler(res, 'Error: Unemployment benefits status is not a boolean');
-		}
+// Q. Simplify the following
 
-		if (clientData.has_unemployment_benefits) {
-			if (!clientData.unemployment_benefits_amount) {
-				return badRequestHandler(res, 'Error: Unemployment benefits amount is not specified');
-			}
+if (clientData.employment_status === ENUMS.EMPLOYMENT_STATUSES.UNEMPLOYED) {
+	if (clientData.has_unemployment_benefits === null || clientData.has_unemployment_benefits === ''
+		|| clientData.has_unemployment_benefits === undefined) {
+		return badRequestHandler(res, 'Error: Unemployment benefits status is not specified');
+	} else if (typeof clientData.has_unemployment_benefits !== 'boolean') {
+		return badRequestHandler(res, 'Error: Unemployment benefits status is not a boolean');
+	}
+
+	if (clientData.has_unemployment_benefits) {
+		if (!clientData.unemployment_benefits_amount) {
+			return badRequestHandler(res, 'Error: Unemployment benefits amount is not specified');
 		}
+	}
 }
 
+// Q. Simplify the following
+
 if (clientData.has_mortgage === null || clientData.has_mortgage === ''
-			|| clientData.has_mortgage === undefined) {
-  return badRequestHandler(res, 'Error: Mortgage status is not specified');
+	|| clientData.has_mortgage === undefined) {
+	return badRequestHandler(res, 'Error: Mortgage status is not specified');
 } else if (typeof clientData.has_mortgage !== 'boolean') {
-  return badRequestHandler(res, 'Error: Mortgage status is not a boolean');
+	return badRequestHandler(res, 'Error: Mortgage status is not a boolean');
 }
 
 if (clientData.has_mortgage) {
-  if (!clientData.mortgage_company_name) {
-    return badRequestHandler(res, 'Error: Mortgage company name is not specified');
+	if (!clientData.mortgage_company_name) {
+		return badRequestHandler(res, 'Error: Mortgage company name is not specified');
   }
 
   if (!clientData.mortgage_payment) {
